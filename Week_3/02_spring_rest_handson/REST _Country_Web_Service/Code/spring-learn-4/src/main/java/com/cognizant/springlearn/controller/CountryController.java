e0b9866cbbfd3ffff9f0adea0479b058be5851fc
sivapallavi18 <sivapallavigundla@gmail.com>
2026-07-01 10:42:55 +0530

    Add files via upload

---
+++ Week_3/02_spring_rest_handson/REST _Country_Web_Service/Code/spring-learn-4/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,22 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        return countryService.getCountry("in");
+
+    }
+}
